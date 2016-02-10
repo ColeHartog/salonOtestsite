7f34b0fcbf682ef3f2bd8077a902ba5be58a7659
Cole Hartog <agentcole7@gmail.com>
2016-02-10 11:23:22 -0700

    working on adminSale layout

--- public/js/adminPage/adminCtrl.js
+++ public/js/adminPage/adminCtrl.js
@@ -13,6 +13,35 @@ angular.module('salonOApp')
     
     $scope.paramFinder = function(){
         return $state.current.url;
-    }
+    };
+    
+    $scope.getClients = function(){
+        adminService.getClients().then(function(response){
+            $scope.clients = response;
+        })
+    };
+    
+    $scope.getClients();
+    
+    $scope.getTeam = function(){
+        adminService.getTeam().then(function(response){
+            $scope.team = response;
+        })
+    };
+    
+    $scope.addNewClient = function(data){
+        adminService.addNewClient(data).then(function(response){
+            $scope.newC ={};
+            $scope.getClients();
+        })
+    };
+    
+    $scope.getProducts = function(){
+        adminService.getProducts().then(function(response){
+            $scope.products = response;
+        })
+    };
+    
+    $scope.getProducts();
     
 })
--- public/js/adminPage/adminService.js
+++ public/js/adminPage/adminService.js
@@ -14,4 +14,46 @@ angular.module('salonOApp')
         })
     };
     
+    this.getClients = function(){
+        return $http({
+            method: 'GET',
+            url: '/api/client'
+        }).then(function(response){
+            return response.data;
+        })
+    };
+    
+    this.getTeam = function(){
+        return $http({
+            method: "GET",
+            url: '/api/stylist'
+        }).then(function(response){
+            return response.data;
+        })
+    };
+    
+    this.addNewClient = function(data){
+        return $http({
+            method: "POST",
+            url: '/api/client',
+            data: {
+                firstname: data.firstname,
+                lastname: data.lastname,
+                email: data.email,
+                phone: data.phone
+            }
+        }).then(function(response){
+            return response.data;
+        })
+    };
+    
+    this.getProducts = function(){
+        return $http({
+            method: 'GET',
+            url: '/api/product'
+        }).then(function(response){
+            return response.data;
+        })
+    };
+    
 })
